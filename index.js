29f972bdfdc621826277c1ee5d69e75c0397d7ea
david <davmwa90@gmail.com>
2022-11-25 17:08:35 +0300

    Defined one function expression and two arrow functions

---
+++ index.js
@@ -0,0 +1,10 @@
+// defining a function expression; 
+const divide = function() {
+    return 2000/100; 
+}
+
+// defining an arrow function; 
+const square = (a = 2) => (a * a); 
+
+// defining another arrow function; 
+const add = (a, b) => (a + b); 
